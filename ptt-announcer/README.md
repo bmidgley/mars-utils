311f006578b5115a704833de762bb695ad763f53
Brad Midgley <bmidgley@gmail.com>
2023-03-08 20:06:20 -0700

    ptt-announcer

---
+++ ptt-announcer/README.md
@@ -0,0 +1,8 @@
+Texts out a message when ptt is pressed or released.
+
+Runs on an esp8266 with micropython installed.
+
+Connections
+* A0 to 3.5mm baofeng outer ring
+* GND to 2.5mm baofeng outer ring
+* serial port on esp8266 to pins on t-beam configured for text messages over serial
---
+++ ptt-announcer/boot.py
@@ -0,0 +1,32 @@
+# install as boot.py on micropython device so it's run on boot
+# monitors the analog input to see when the ptt button is pressed
+
+import uos, machine
+import gc
+gc.collect()
+
+from machine import Pin, ADC
+from time import sleep
+
+led = machine.Pin(2, Pin.OUT)
+ptt = machine.ADC(0)
+
+led(1)
+
+transmitting = False
+
+while True:
+    ptt_value = ptt.read()
+    if ptt_value < 30:
+        if transmitting == False:
+            transmitting = True
+            led(0)
+            print("-talking")
+    else:
+        if transmitting == True:
+            transmitting = False
+            led(1)
+            print("+talking")
+      
+    sleep(0.2)
+
