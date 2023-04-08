f4461046550a622dccf450125d123cfcf9c0718d
Brad Midgley <bmidgley@gmail.com>
2023-04-08 10:14:45 -0600

    lint

--- audio/detect.py
+++ audio/detect.py
@@ -23,6 +23,14 @@ def save_audio(frames):
     print(f'wrote {filename} with {len(frames)} frames')
 
 audio=pyaudio.PyAudio()
+
+info = audio.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+
+for i in range(0, numdevices):
+    if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i))
+
 stream=audio.open(format=FORMAT,channels=CHANNELS,
                   rate=RATE,
                   input=True,
@@ -35,9 +43,9 @@ while True:
 
     while len(frames) == 0:
         for i in range(0,int(RATE/CHUNK*RECORD_SECONDS)):
-            data=stream.read(CHUNK, exception_on_overflow = False)
-            data_chunk=array('h', data)
-            vol=max(data_chunk)
+            data = stream.read(CHUNK, exception_on_overflow = False)
+            data_chunk = array('h', data)
+            vol = max(data_chunk)
             if vol > int(sys.argv[1]) or heard:
                 if not heard: print("heard")
                 heard = True
