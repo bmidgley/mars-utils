45482f2ed477b2d5c5103b068231aa46fcfda455
Brad Midgley <bmidgley@gmail.com>
2023-05-04 08:45:14 -0600

    break gpx up into different walks

--- mq2gpx.py
+++ mq2gpx.py
@@ -6,9 +6,41 @@
 import sys
 import json
 import xmltodict
+from datetime import datetime
+import dateutil.parser
 
 stations = {}
 points = {}
+samples = -1
+
+def write_points(station_name, points):
+    if points == []: return
+    gpx = {
+        'gpx': {
+            '@xmlns': "http://www.topografix.com/GPX/1/1", 
+            '@xmlns:gpxx': "http://www.garmin.com/xmlschemas/GpxExtensions/v3", 
+            '@xmlns:gpxtpx': "http://www.garmin.com/xmlschemas/TrackPointExtension/v1", 
+            '@creator': "Oregon 400t", 
+            '@version': "1.1", 
+            '@xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance", 
+            '@xsi:schemaLocation': "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd",
+            'trk': {
+                'name': station_name,
+                'trkseg': {
+                    'trkpt': 
+                        points
+                }
+            }
+        }
+    }
+    # print(points[0])
+    with open(f'{station_name}-{points[0]["time"]}.gpx', 'w') as gpx_file:
+        gpx_file.write(xmltodict.unparse(gpx, pretty=True))
+
+def seconds_apart(t1, t2):
+    d1=dateutil.parser.isoparse(t1.replace('Z',''))
+    d2=dateutil.parser.isoparse(t2.replace('Z',''))
+    return((d2 - d1).total_seconds())
 
 for line in sys.stdin:
     full_message = json.loads(line)
@@ -25,26 +57,20 @@ for line in sys.stdin:
         }
         if 'altitude' in message['payload']:
             entry['ele'] = message['payload']['altitude']
-        points[station_name].append(entry)
+
+        if samples != 0:
+            samples -= 1
+            points[station_name].append(entry)
 
 for station_name in points:
-    gpx = {
-        'gpx': {
-            '@xmlns': "http://www.topografix.com/GPX/1/1", 
-            '@xmlns:gpxx': "http://www.garmin.com/xmlschemas/GpxExtensions/v3", 
-            '@xmlns:gpxtpx': "http://www.garmin.com/xmlschemas/TrackPointExtension/v1", 
-            '@creator': "Oregon 400t", 
-            '@version': "1.1", 
-            '@xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance", 
-            '@xsi:schemaLocation': "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd",
-            'trk': {
-                'name': station_name,
-                'trkseg': {
-                    'trkpt': 
-                        points[station_name]
-                }
-            }
-        }
-    }
-    with open(f'{station_name}.gpx', 'w') as gpx_file:
-        gpx_file.write(xmltodict.unparse(gpx, pretty=True))
+    spoints = points[station_name]
+
+    # break points[station_name] up by clusters with no more than one hour between samples
+    while len(spoints) > 0:
+        pslice = []
+        pslice.append(spoints.pop(0))
+
+        while len(spoints) > 0 and seconds_apart(pslice[-1]['time'], spoints[0]['time']) < 60 * 60:
+            pslice.append(spoints.pop(0))
+
+        write_points(station_name, pslice)
