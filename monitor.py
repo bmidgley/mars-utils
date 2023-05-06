e13e7adc0fdca08c504f993a9b3b50258b437659
Brad Midgley <bmidgley@gmail.com>
2023-05-06 17:52:48 -0600

    show distance from hab

--- monitor.py
+++ monitor.py
@@ -5,8 +5,16 @@
 
 import sys
 import json
+import geopy.distance
 
+ignore_points = [{"@lat": 38.405744, "@lon": -110.792172}, {"@lat": 38.4064465, "@lon": -110.791946}]
 stations = {1439117596: 'RadGateWay', -1951726776: 'Astro2-MDRS', -240061613: 'Astro1-MDRS'}
+hab = ignore_points[1]
+
+def distance(p1, p2):
+    tup1 = (p1["@lat"], p1["@lon"])
+    tup2 = (p2["@lat"], p2["@lon"])
+    return geopy.distance.geodesic(tup1, tup2).meters
 
 print(f'{"time".ljust(15)}{"name".ljust(15)}{"battery".ljust(15)}{"temperature".ljust(15)}{"humidity".ljust(15)}{"location"}')
 for line in sys.stdin:
@@ -29,7 +37,8 @@ for line in sys.stdin:
                 alt = message['payload']['altitude']
             else:
                 alt = None
-            print(f'{tst.ljust(15)}{station_name.ljust(15)}{"".ljust(45)}http://maps.google.com/maps?z=12&t=k&q=loc:{lat}+{lon}')
+            dist = distance(hab, {"@lat": lat, "@lon": lon})
+            print(f'{tst.ljust(15)}{station_name.ljust(15)}{"".ljust(45)}{int(dist)} http://maps.google.com/maps?z=12&t=k&q=loc:{lat}+{lon}')
         if message['type'] == 'telemetry' and station_name:
             if 'battery_level' in message['payload']:
                 battery = message['payload']['battery_level']
