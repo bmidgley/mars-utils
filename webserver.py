d6492a5b81e1b66ba59ec63bbd2073872b687b49
Brad Midgley <bmidgley@gmail.com>
2023-06-18 00:03:54 -0600

    store text messages in json

--- webserver.py
+++ webserver.py
@@ -7,6 +7,7 @@
 
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from threading import Thread
+from datetime import datetime
 import sys
 import json
 import geopy.distance
@@ -56,8 +57,8 @@ def distance(p1, p2):
 def main(line):
     full_message = json.loads(line)
     message = full_message['payload']
-    tst = full_message['tst'][11:19]
-    iso = full_message['tst'].replace('Z', '')
+    iso = datetime.fromtimestamp(message['timestamp']).isoformat()
+    tst = iso[11:19]
     if message['from'] in stations:
         station_name = stations[message['from']]
         if not station_name in response: response[station_name] = {}
@@ -68,7 +69,7 @@ def main(line):
             stations[message['from']] = message['payload']['longname']
             if 'hardware' in message['payload']: hardware_type[message['from']] = message['payload']['hardware']
             station_name = message['payload']['longname']
-            print(f'{tst.ljust(15)}{station_name.ljust(15)}')
+            print(f'{tst.ljust(15)}{station_name.ljust(16)}')
         if message['type'] == 'position' and station_name:
             lat = message['payload']['latitude_i'] / 10000000
             lon = message['payload']['longitude_i']  / 10000000
@@ -77,7 +78,7 @@ def main(line):
             else:
                 alt = None
             dist = distance(hab, {"@lat": lat, "@lon": lon})
-            print(f'{tst.ljust(15)}{station_name.ljust(15)}{"".ljust(45)}{int(dist)} http://maps.google.com/maps?z=12&t=k&q=loc:{lat}+{lon}')
+            print(f'{tst.ljust(15)}{station_name.ljust(16)}{"".ljust(45)}{int(dist)} http://maps.google.com/maps?z=12&t=k&q=loc:{lat}+{lon}')
             if lat != 0 and lon != 0:
                 hw = -1
                 if message['from'] in hardware_type: hw = hardware_type[message['from']]
@@ -87,15 +88,21 @@ def main(line):
         if message['type'] == 'telemetry' and station_name:
             if 'battery_level' in message['payload']:
                 battery = message['payload']['battery_level']
-                print(f'{tst.ljust(15)}{station_name.ljust(15)}{battery}%')
+                print(f'{tst.ljust(15)}{station_name.ljust(16)}{battery}%')
                 response[station_name].update({"battery": battery})
             if 'relative_humidity' in message['payload'] and 'temperature' in message['payload']:
                 temperature = round(message['payload']['temperature'])
                 temperature_string = f'{temperature}c'
                 relative_humidity = round(message['payload']['relative_humidity'])
                 relative_humidity_string = f'{relative_humidity}%'
-                print(f'{tst.ljust(15)}{station_name.ljust(15)}{"".ljust(15)}{temperature_string.ljust(15)}{relative_humidity_string.ljust(15)}')
+                print(f'{tst.ljust(15)}{station_name.ljust(16)}{"".ljust(15)}{temperature_string.ljust(15)}{relative_humidity_string.ljust(15)}')
                 response[station_name].update({"temperature": temperature, "humidity": relative_humidity})
+        if message['type'] == 'text' and station_name:
+            text = message['payload']['text']
+            print(f'{tst.ljust(15)}{station_name.ljust(16)}{"".ljust(45)}{text}')
+            if 'text' not in response[station_name]:
+                response[station_name]['text'] = {}
+            response[station_name]['text'].update({iso: text})
 
 if __name__ == "__main__":
     webServer = ThreadingHTTPServer((hostName, int(serverPortString)), MyServer)
@@ -115,5 +122,5 @@ if __name__ == "__main__":
     thread.daemon = True
     thread.start()
 
-    print(f'{"time".ljust(15)}{"name".ljust(15)}{"battery".ljust(15)}{"temperature".ljust(15)}{"humidity".ljust(15)}{"location"}')
+    print(f'{"time".ljust(15)}{"name".ljust(16)}{"battery".ljust(15)}{"temperature".ljust(15)}{"humidity".ljust(15)}{"location"}')
     for line in sys.stdin: main(line)
