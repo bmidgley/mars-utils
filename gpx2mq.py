02eb471b9150ea2721faa04a38c69ab0d05b185a
Brad Midgley <bmidgley@gmail.com>
2024-01-17 22:13:43 -0700

    play waypoints into gpx2mq

--- gpx2mq.py
+++ gpx2mq.py
@@ -15,12 +15,17 @@ import random
 from datetime import datetime
 from dateutil import parser
 
+def get_position(point, adjustment):
+  timestamp = parser.parse(point['time']) + adjustment
+  seconds = int(timestamp.timestamp())
+  return timestamp, seconds, {'from': '1', 'type': 'position', 'timestamp': seconds, 'payload': {'hardware': 7,
+          'latitude_i': int(float(point['@lat'])*10000000),
+          'longitude_i': int(float(point['@lon'])*10000000),
+          'elevation': int(float(point['ele']))}}
+
 def inject_file(filename):
     with open(filename, 'r') as gpx_file:
         data = xmltodict.parse(gpx_file.read())
-
-    # todo: send waypoints to bus, also make sure waypoints are present first
-    #waypoints = data['gpx']['wpt']
 
     trk = data['gpx']['trk']
     code = random.randint(10, 30)
@@ -31,15 +36,24 @@ def inject_file(filename):
     start_time = parser.parse(points[0]['time'])
     adjustment = datetime.now(pytz.utc) - start_time
 
+    # send waypoints to bus as location and text
+    waypoints = []
+    if 'wpt' in data['gpx']:
+      for point in data['gpx']['wpt']:
+        timestamp, seconds, position = get_position(point, adjustment)
+        waypoints.append(position)
+        waypoints.append({'from': '1', 'type': 'text', 'timestamp': seconds, 'payload': {'text': point['name'], 'hardware': 0}})
+    print(f'waypoints: {waypoints}')
+
     for point in points:
-        timestamp = parser.parse(point['time']) + adjustment
-        seconds = int(timestamp.timestamp())
+        print(point)
+        timestamp, seconds, position = get_position(point, adjustment)
         nodeinfo = {'from': '1', 'type': 'nodeinfo', 'timestamp': seconds, 'payload': {'longname': name, 'hardware': 7}}
-        position = {'from': '1', 'type': 'position', 'timestamp': seconds, 'payload': {'hardware': 7,
-            'latitude_i': int(float(point['@lat'])*10000000),
-            'longitude_i': int(float(point['@lon'])*10000000),
-            'elevation': int(float(point['ele']))}
-        }
+        while len(waypoints) > 0 and waypoints[0]['timestamp'] < seconds:
+          message = waypoints.pop(0)
+          while(message['timestamp'] > datetime.now(pytz.utc)): time.sleep(1)
+          print(message)
+          os.system(f'{send_cmd} -m {json.dumps(json.dumps(message))}')
         while(timestamp > datetime.now(pytz.utc)): time.sleep(1)
         for message in [nodeinfo, position]:
             print(message)
